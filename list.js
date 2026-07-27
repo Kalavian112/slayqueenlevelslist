fe0ca13c677687569a753c5fef9d9ad6d16491e8
Kalavian 🏳️‍⚧️ <natalieweber720@proton.me>
2026-07-27 15:48:18 -0400

    Added Storming Summit at 5, added "Trans :3" tier

--- list.js
+++ list.js
@@ -3,48 +3,49 @@ Flag Format
 Bits 0-2 difficulty (sub-demon, easy, medium, hard, insane, extreme, challenge, impossible)
 Bit 3 remake tag
 Bit 4 unrated tag
+Bit 5 platformer tag
+Bit 6 trans tag
 */
 const levels = [
-	// many levels deemed unlistworthy were removed from this version
-	['Dysphoria','IceboxOG','81413188', 4],
+	['Dysphoria','IceboxOG','81413188', 36],
 	['SLAY MOMENT', 'RafaBirds', '118366680', 5],
-	['Pride','Sakura','62444479', 2],
+	['Pride','Sakura','62444479', 34],
 	['The Perfect Girl','Kapelli', '91530507', 21],
-	['Transclysm','Sinneslochen','116804015', 12],
-	['pride level','qjivxn', '70719847', 0],
-	['priDEMONth','tricipital','91677667', 0],
+	['Storming Summit','Nimbus','76653933', 37],
+	['Transclysm','Sinneslochen','116804015', 60],
+	['pride level','qjivxn', '70719847', 32],
+	['priDEMONth','tricipital','91677667', 32],
 	['Femboy Circles','Skeletonette, Akirameru','130633756', 29],
 	['Femboy Fantasy','Sensawa','71798659', 21],
 	['GIRLTRON 3000','BridgetTheCroco', '79037527', 0],
 	['project girlboss','koko43','110769842', 5],
-	['pride','shrympo','70476012', 1],
+	['pride','shrympo','70476012', 33],
 	['Cat Girl', 'Choplox', '108526412', 0],
 	['girl perspective','rinad2023', '114499467', 0],
-	['EstrofemTransventure', 'Kalavian', '120486509', 17],
+	['EstrofemTransventure', 'Kalavian', '120486509', 49],
 	['Femboy Gang','xm1x', '97928486', 22],
 	['slay total', 'rafabirds', '113003780', 0],
-	['Dysphoria', 'Hychie', '70639047', 0],
+	['Dysphoria', 'Hychie', '70639047', 32],
 	['meow hard','meowdead*', '111862459', 5],
-	['prIde','DubbyBall', '93233103', 0],
+	['prIde','DubbyBall', '93233103', 32],
 	['Cat Planet','IcEDCave', '84766628', 5],
-	['transide', 'qalli', '106758929', 40],
-	['Trans Acu','solstacoded', '80142751', 29],
+	['transide', 'qalli', '106758929', 104],
+	['Trans Acu','solstacoded', '80142751', 61],
 	['Cute Girl Feelings','AngelEffect', '109289651', 23],
 	['RAINBOW GIRL','LLoom','103062165', 5],
 	['Catboy Demon','PPPixel', 'Unreleased', 21],
-	['thespikeisoverthere','BranSilver', '58977211', 0],
+	['thespikeisoverthere','BranSilver', '58977211', 32],
 	['Femboy Friday','Narkify','86414534', 21],
-	['Gender Chamber','Kalavian', 'Unreleased', 30],
+	['Gender Chamber','Kalavian', 'Unreleased', 62],
 	['BUNNYGIRLAUBREY', 'Aeqing1 & Illuminati65', '95653592', 23],
 	['Tower of Femboy','ImLaxris', '93365782', 23],
-	['Woke Circles', 'PPPixel', '88318424', 29],
+	['Woke Circles', 'PPPixel', '88318424', 61],
 ];
 const tiers = [
-	// list tiers were simplified in this version until there's more levels
 	[0, 'The Summit'],
 	[7, 'Epic Tier'],
 	[18, 'Chill Tier'],
-	[32, 'UwU Tier'],
+	[32, 'Extended Tier'],
 	[50, 'Legacy List'],
 	[1000, ''] // gotta have this otherwise it'll break
 ];
@@ -65,7 +66,7 @@ function buildList(){
 		}
 		// get level name
 		let levelName = levels[i][2];
-		let nameLen = levelName.length-1
+		let nameLen = levelName.length-1;
 		if(levelName[nameLen]=='*'){
 			levelName = levelName.substring(0, nameLen) + ' and more'
 		}
@@ -82,9 +83,12 @@ function getOtherTags(level){
 		otherTags += '<span class="level-tag">Remake</span>';
 	}
 	if(levels[level][3]&16){
-		return otherTags += '<span class="level-tag">Unrated</span>';
+		otherTags += '<span class="level-tag">Unrated</span>';
 	}
-	if(levels[level][3]>=32){
+	if(levels[level][3]&32){
+		otherTags += '<span class="level-tag">Trans :3</span>';
+	}
+	if(levels[level][3]>=64){
 		return otherTags + '<span class="level-tag">Platformer</span>';
 	}
 	return otherTags;
